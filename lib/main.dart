d85661f823b63fd8ae75df0f8b30543df0b1a16a
Cndynajae <csagitasari@gmail.com>
2024-07-05 16:19:20 +0700

    tugas wisata

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,35 @@ import 'package:projek/listview/list_builder.dart';
 import 'package:projek/listview/list_separated.dart';
 import 'package:projek/listview/list_example.dart';
 
+import 'package:projek/screens/home_screen.dart';
+import 'package:projek/screens/about_screen.dart';
+import 'package:projek/screens/wisata_screen.dart';
+import 'package:projek/screens/detail_wisata_screen.dart';
+
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 255, 0, 0),
-          title: Text("Belajar Flutter"),
-        ),
-        body: Center(
-          child: ListExample(),
-        ),
+      routes:{
+        '/about': (context) => AboutScreen(),
+        '/home': (context)=> HomeScreen(),
+        '/wisata': (context)=> WisataScreen()
+      },
+      initialRoute: '/home',
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: HomeScreen()
     );
   }
 }
+
